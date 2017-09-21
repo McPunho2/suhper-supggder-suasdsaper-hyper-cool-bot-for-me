20b6d302b2dd079a3777a087343b3e2d003ffa8f
McPunho2 <32145610+McPunho2@users.noreply.github.com>
2017-09-21 14:29:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const YTDL = require("ytdl-core");
 const PREFIX = ">"
 
 function generateHex() {
-    return "#" + Math.floor(Math.random()*16777215).toString(16);
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 function play(connection, message) {
     var server = servers[message.guild.id];
 
-    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: audioonly}));
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], { filter: audioonly }));
 
     server.queue.shift();
 
-    server.dispatcher.on("end", function() {
+    server.dispatcher.on("end", function () {
         if (server.queue[0]) play(connection, message)
         else connection.disconnect();
     });
@@ -32,18 +32,21 @@ var fortunes = [
     "My master is Kyuubi#1669 (McPunho2)",
     "What?",
     "...",
-    "PHIJET PHINNNNEEETZZZZZZZZZZZZZZZZZZZZZ"
+    "PHIJET PHINNNNEEETZZZZZZZZZZZZZZZZZZZZZ",
+    "Nazism is good",
+    "I refuse to say anything",
+    "I am a good bot, controlled by a program."
 ];
 
 var bot = new Discord.Client();
 
 var servers = {};
 
-bot.on("ready", function() {
+bot.on("ready", function () {
     console.log("Ready");
 });
 
-bot.on("guildMemberAdd", function(member) {
+bot.on("guildMemberAdd", function (member) {
     member.guild.channels.find("name", "scripts_nudes_everything").sendMessage(member.toString() + " Welcome!")
 
     member.addRole(member.guild.roles.find("name", "dank member"));
@@ -52,84 +55,84 @@ bot.on("guildMemberAdd", function(member) {
         name: member.user.username,
         color: generateHex(),
         permissions: []
-    }).then(function(role) {
+    }).then(function (role) {
         member.addRole(role);
     })
 });
 
-bot.on("message", function(message) {
+bot.on("message", function (message) {
     if (message.author.equals(bot.user)) return;
 
     if (!message.content.startsWith(PREFIX)) return;
 
     var args = message.content.substring(PREFIX.length).split(" ");
 
-    switch(args[0].toLowerCase()) {
-    case "cmds":
-       var cmds = new Discord.RichEmbed()
-       .addField("cmds", "Show the commands")
-       .addField("ping", "I will be rude")
-       .addField("noticeme", "You will get noticed")
-       .addField("info", "Info!")
-       .addField("8ball", "Talk with me atleast a bit...")
-       .setColor(0xDC143C)
-       .setThumbnail()
-        message.channel.sendEmbed(cmds);
-        break;
-    case "removerole":
-        message.member.removeRole(message.member.guild.roles.find("name", "dank member"));
-        break;
-    case "deleterole":
-        message.member.guild.roles.find("name", "dank member").delete();
-        break;
-    case "play":
-        if (!args[1]) {
-            message.channel.sendMessage("Please provide a link.")
-            return;
-        }
+    switch (args[0].toLowerCase()) {
+        case "cmds":
+            var cmds = new Discord.RichEmbed()
+                .addField("cmds", "Show the commands")
+                .addField("ping", "I will be rude")
+                .addField("noticeme", "You will get noticed")
+                .addField("info", "Info!")
+                .addField("8ball", "Talk with me atleast a bit...")
+                .setColor(0xDC143C)
+                .setThumbnail()
+            message.channel.sendEmbed(cmds);
+            break;
+        case "removerole":
+            message.member.removeRole(message.member.guild.roles.find("name", "dank member"));
+            break;
+        case "deleterole":
+            message.member.guild.roles.find("name", "dank member").delete();
+            break;
+        case "die":
+            if (!args[1]) {
+                message.channel.sendMessage("Please provide a link.")
+                return;
+            }
 
-        if (!message.member.voiceChannel) {
-            message.channel.sendMessage("You must be in a voice channel.")
-            return;
-        }
+            if (!message.member.voiceChannel) {
+                message.channel.sendMessage("You must be in a voice channel.")
+                return;
+            }
 
-        if(!server[message.guild.id]) server[message.guild.id] = {
-            queue: {}
-        };
-        
-        var server = server[message.guild.id];
+            if (!server[message.guild.id]) server[message.guild.id] = {
+               queue: {} 
+            };
 
-        server.queue.push(args[1]);
+            var server = server[message.guild.id];
 
-        if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
-            play(connection, message)
-        })
-        break;
-    case "skip":
-        var server = servers[message.guild.id];
+            server.queue.push(args[1]);
 
-        if (server.dispatcher) server.dispatcher.end();
-        break;
-    case "stop":
-        var server = servers[message.guild.id];
+            if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function (connection) {
+                play(connection, message)
+            })
+            break;
+        case "skip":
+            var server = servers[message.guild.id];
 
-        if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
-        break;
-    case "noticeme":
-    message.channel.sendMessage(message.author.toString() + " You were noticed by the god of memes, now get the fuck out.")
-    break;
-    case "ping":
-        message.channel.sendMessage("Fuck you, i am not saying pong");
-        break;
-    case "info":
-        message.channel.sendMessage("I am a meme bot, created for fun by McPunho2 (Kyuubi#1669).");
-        break;
-    case "8ball":
-        if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
-        else message.channel.sendMessage("I can't read that");
-        break;
-    default:
-        message.channel.sendMessage("Invalid command, say **>cmds** for commands.");
+            if (server.dispatcher) server.dispatcher.end();
+            break;
+        case "stop":
+            var server = servers[message.guild.id];
+
+            if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+            break;
+        case "noticeme":
+            message.channel.sendMessage(message.author.toString() + " You were noticed by the god of memes, now get the fuck out.")
+            break;
+        case "ping":
+            message.channel.sendMessage("Fuck you, i am not saying pong");
+            break;
+        case "info":
+            message.channel.sendMessage("I am a meme bot, created for fun by McPunho2 (Kyuubi#1669).");
+            break;
+        case "8ball":
+            if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
+            else message.channel.sendMessage("I can't read that");
+            break;
+        default:
+            message.channel.sendMessage("Invalid command, say **>cmds** for commands.");
     }
 
 });
