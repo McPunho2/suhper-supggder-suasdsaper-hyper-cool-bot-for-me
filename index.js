cd2b20eae0a6059a6f18ceede6c63bd5cac322ff
McPunho2 <32145610+McPunho2@users.noreply.github.com>
2017-09-23 16:54:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ bot.on("message", function (message) {
             });
             break;
         case "say":
-        const sayMessage = args.join(" ");
+        const sayMessage = args.join("");
         message.delete().catch(O_o=>{}); 
         message.channel.send(sayMessage);
         break;
@@ -88,6 +88,14 @@ bot.on("message", function (message) {
             break;
         case "info":
             message.channel.sendMessage("I am a meme bot, created for fun by McPunho2 (Kyuubi#1669).");
+            break;
+        case "purge":
+            const deleteCount = parseInt(args[0], 10);
+            if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+            return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+            const fetched = await message.channel.fetchMessages({count: deleteCount});
+            message.channel.bulkDelete(fetched)
+              .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
             break;
         case "8ball":
             if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
