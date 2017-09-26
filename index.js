33a608011fadde58afe02acc84cb86f19aed5617
McPunho2 <32145610+McPunho2@users.noreply.github.com>
2017-09-26 14:42:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -68,19 +68,9 @@ bot.on("message", function (message) {
                 message.channel.send("Ou nooo i can't do anythingo");
             });
             break;
-        case "purge":
-             const deleteCount = parseInt(args[0], 10);
-  
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-   async const fetched = await message.channel.fetchMessages({count: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-        break;
         case "say":
         const sayMessage = args.join(" ");
-        message.delete()
+        message.delete().catch(O_o=>{}); 
         message.channel.send(sayMessage);
         break;
         case "ban":
