9bc692ce2a625c4a577ea07ddaf0319b3c5da1f4
McPunho2 <32145610+McPunho2@users.noreply.github.com>
2017-09-26 14:08:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -58,31 +58,61 @@ bot.on("message", function (message) {
 
     var args = message.content.substring(PREFIX.length).split(" ");
 
-    var moron= message.mentions.members.first();
-
     switch (args[0].toLowerCase()) {
         case "kick":
-            moron.kick().then((member) => {
-                    message.channel.send(":wave: " + moron.displayName + " has been sucessfully kicked :point_right:");
-            }).catch(() => {
-                message.channel.send("Acess Denied");
-            });
+            if(!message.member.roles.some(r=>["Administrator", "Moderator", "Admin"].includes(r.name)) )
+                return message.reply("Fock yu! U dun have permison!");
+
+                let member = message.mentions.members.first();
+                if(!member)
+                    return message.reply("Please mention a valid member of this server");
+                if(!member.kickable)
+                    return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+                let reason = args.slice(1).join(' ');
+                if(!reason)
+                    return message.reply("Please indicate a reason for the kick!");
+                await member.kick(reason)
+                    .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+                message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
             break;
         case "say":
-        const sayMessage = args.join("");
+        const sayMessage = args.join(" ");
         message.delete().catch(O_o=>{}); 
         message.channel.send(sayMessage);
         break;
         case "ban":
-            moron.ban().then((member) => {
-                message.channel.send(moron.displayName + " is an idiot and was banned by " + message.author.toString());
-            }).catch(() => {
-                message.channel.send("Acess denied sori.");
-            });
-            break;
+        if(!message.member.roles.some(r=>["Administrator", "Admin"].includes(r.name)) )
+            return message.reply("Sorry, you don't have permissions to use this!");
+         break;
+
+         let member = message.mentions.members.first();
+
+         if(!member)
+            return message.reply("Please mention a valid member of this server");
+         if(!member.bannable) 
+            return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+        
+            let reason = args.slice(1).join(' ');
+            if(!reason)
+              return message.reply("Please indicate a reason for the ban!");
+            await member.ban(reason)
+              .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+            message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
         case "noticeme":
             message.channel.sendMessage(message.author.toString() + " You were noticed by the god of memes, now get the fuck out.")
             break;
+        case "purge":
+        const deleteCount = parseInt(args[0], 10);
+        
+        // Ooooh nice, combined conditions. <3
+        if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+          return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+        
+        // So we get our messages, and delete them. Simple enough, right?
+        const fetched = await message.channel.fetchMessages({count: deleteCount});
+        message.channel.bulkDelete(fetched)
+          .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+        break;
         case "ping":
             message.channel.sendMessage("Fuck you, i am not saying pong");
             break;
