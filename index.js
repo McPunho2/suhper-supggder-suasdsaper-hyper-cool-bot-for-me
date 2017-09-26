8232f6c1ae9f39fa6f8fba4cea3d362342b53af2
McPunho2 <32145610+McPunho2@users.noreply.github.com>
2017-09-26 14:11:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -62,15 +62,17 @@ bot.on("message", function (message) {
         case "kick":
             if(!message.member.roles.some(r=>["Administrator", "Moderator", "Admin"].includes(r.name)) )
                 return message.reply("Fock yu! U dun have permison!");
-
+                //meingotch\\
                 let member = message.mentions.members.first();
                 if(!member)
                     return message.reply("Please mention a valid member of this server");
                 if(!member.kickable)
                     return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
                 let reason = args.slice(1).join(' ');
+                
                 if(!reason)
                     return message.reply("Please indicate a reason for the kick!");
+                //wiagfuckyou\\
                 await member.kick(reason)
                     .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
                 message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
