4055eadf4207cbc7444ae068b3be4169017c52e8
McPunho2 <32145610+McPunho2@users.noreply.github.com>
2017-09-26 20:42:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -51,43 +51,52 @@ bot.on("guildMemberAdd", function (member) {
     member.guild.defaultChannel.send("Welcome to " + member.guild.name + " " + member.toString())
 });
 
-client.on("message", async message => {
+bot.on("message", async message => {
     
-    if(message.author.bot) return;
+  if(message.author.bot) return;
+  
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+   
+  if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    const m = await message.channel.send("Ping?");
+    m.edit(`Fuck you i am not saying pong. Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+  
+  if(command === "say") {
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+  }
+  
+  if(command === "kick") {
+    if(!message.member.roles.some(r=>["Admin", "Owner/Creator/God"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
     
-     if(message.content.indexOf(PREFIX) !== 0) return;
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+    
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the kick!");
+    
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
 
-    const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
-    
-    switch (args[0].toLowerCase()) {
-        case "kick":
-            if(!message.member.roles.some(r=>["Administrator", "Owner/Creator/God", "Admin"].includes(r.name)) )
-                return message.reply("Sorry, you don't have permissions to use this!"); 
-            
-            let member = message.mentions.members.first();
-            if(!member)
-                return message.reply("Please mention a valid member of this server");
-            if(!member.bannable) 
-                return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
-            
-            let reason = args.slice(1).join(' ');
-            if(!reason)
-                return message.reply("Please indicate a reason for the ban!");
-            if(!member.bannable) 
-                return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
-            await member.ban(reason)
-              .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-            message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-            
-            break;
-        case "say":
-        const sayMessage = args.join(" ");
-        message.delete().catch(O_o=>{}); 
-        message.channel.send(sayMessage);
-        break;
-        case "ban":
-            if(!message.member.roles.some(r=>["Administrator", "Owner/Creator/God", "Admin"].includes(r.name)) )
-               return message.reply("Sorry, you don't have permissions to use this!");
+  }
+  
+  if(command === "ban") {
+      
+    if(!message.member.roles.some(r=>["Administrator", "Owner/Creator/God"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
     
     let member = message.mentions.members.first();
     if(!member)
@@ -103,25 +112,30 @@ client.on("message", async message => {
       .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
     message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
   }
-            break;
-        case "noticeme":
-            message.channel.sendMessage(message.author.toString() + " You were noticed by the god of memes, now get the fuck out.")
-            break;
-        case "ping":
-            const await m = message.channel.send("Ping?");
-            m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
-            break;
-        case "info":
-            message.channel.sendMessage("I am a meme bot, created for fun by McPunho2 (Kyuubi#1669).");
-            break;
-        case "8ball":
-            if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
-            else message.channel.sendMessage("I can't read that");
-            break;
-        default:
-            message.channel.sendMessage("Invalid command, say **>cmds** for commands.");
-    }
-
+  
+  if(command === "purge") {
+    
+    const deleteCount = parseInt(args[0], 10);
+    
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
+  if(command === "8ball") {
+      if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
+      else message.channel.sendMessage("I can't read that");
+  }
+  if(command === "info") {
+      message.channel.sendMessage("I am a meme bot, created for fun by McPunho2 (Kyuubi#1669).");
+  }
+  if(command === "noticeme") {
+      message.channel.sendMessage(message.author.toString() + " You were noticed by the god of memes, now get the fuck out.")
+  }
+  default:
+    message.channel.sendMessage("Invalid command, say **>cmds** for commands.");
 });
 
 bot.login(process.env.TOKEN);
